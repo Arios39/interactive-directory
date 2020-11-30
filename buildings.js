b44adeb5c337b9b93e30376387ece5f1f7931711
Andres Rios <arios39@ucmerced.edu>
2020-11-30 08:43:26 -0800

    Add files via upload

---
+++ buildings.js
@@ -0,0 +1,654 @@
+var buildings = {
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.424197,
+            37.36476
+          ]
+        },
+        "properties": {
+          "Name": "Sustainability Research and Engineering"
+        }
+      }, {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.423864,
+            37.363805
+          ]
+        },
+        "properties": {
+          "Name": "Arts and Computational Sciences"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.422558,
+            37.364654
+          ]
+        },
+        "properties": {
+          "Name": "Biomedical Sciences and Physics"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.424784,
+            37.363156145393745
+          ]
+        },
+        "properties": {
+          "Name": "Granite Pass"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.424784,
+            37.363156145393745
+          ]
+        },
+        "properties": {
+          "Name": "Glacier Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42563781264228,
+            37.362481547371544
+          ]
+        },
+        "properties": {
+          "Name": "Sentinel Rock"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.426426,
+            37.361496
+          ]
+        },
+        "properties": {
+          "Name": "El Portal"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.424776,
+            37.364102
+          ]
+        },
+        "properties": {
+          "Name": "The Pavilion"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.428464,
+            37.361784
+          ]
+        },
+        "properties": {
+          "Name": "Health and Athletic Center"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.427099,
+            37.362259
+          ]
+        },
+        "properties": {
+          "Name": "Administration Building"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.427233,
+            37.362774
+          ]
+        },
+        "properties": {
+          "Name": "Conference Center"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.43113877083411,
+      37.363795535159966
+          ]
+        },
+        "properties": {
+          "Name": "Early Childhood Education Center"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.4302637772657,
+      37.36360136553159
+          ]
+        },
+        "properties": {
+          "Name": "Cathedral"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42975250782979,
+            37.364091183951714
+          ]
+        },
+        "properties": {
+          "Name": "Half Dome"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.4294956822767,
+            37.36360766939501
+          ]
+        },
+        "properties": {
+          "Name": "Tenaya"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42874782453771,
+      37.36412479347243
+          ]
+        },
+        "properties": {
+          "Name": "Stanislaus Hall"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42875031739698,
+      37.363855334635716
+          ]
+        },
+        "properties": {
+          "Name": "Madera Hall"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42875281025583,
+      37.36358191217985
+          ]
+        },
+        "properties": {
+          "Name": "Tulare Hall"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42835529316082,
+      37.36409415780233
+          ]
+        },
+        "properties": {
+          "Name": "Fresno Hall"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42823314306176,
+            37.36386036259482
+          ]
+        },
+        "properties": {
+          "Name": "Kings Hall"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.428235635921,
+            37.36360675341099
+          ]
+        },
+        "properties": {
+          "Name": "Kern Hall"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42855367993397,
+            37.36443469410847
+          ]
+        },
+        "properties": {
+          "Name": "San Joaquin Hall"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42822960824304,
+            37.36465858103985
+          ]
+        },
+        "properties": {
+          "Name": "Calaveras Hall"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42802569881027,
+            37.36448221974155
+          ]
+        },
+        "properties": {
+          "Name": "Merced Hall"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42742545669306,
+      37.36409217648968
+          ]
+        },
+        "properties": {
+          "Name": "Terrace Center"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42708538589139,
+            37.36456149741841
+          ]
+        },
+        "properties": {
+          "Name": "Wallace Dining Center"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42678858577735,
+            37.36505686046384
+          ]
+        },
+        "properties": {
+          "Name": "Tuolumne Hall"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42761621501933,
+      37.36531442730687
+          ]
+        },
+        "properties": {
+          "Name": "Mariposa Hall"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42622205126669,
+      37.36537887018662
+          ]
+        },
+        "properties": {
+          "Name": "Joseph E. Gallo Recreation & Wellness Center"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42682532318722,
+      37.365858338008465
+          ]
+        },
+        "properties": {
+          "Name": "Student Activities and Athletics Center"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42461491133255,
+            37.3663606603549
+          ]
+        },
+        "properties": {
+          "Name": "Kolligian Library"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42453264434266,
+      37.367014491755114
+          ]
+        },
+        "properties": {
+          "Name": "Classroom and Office Building 2"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42341244219365,
+            37.367058469471715
+          ]
+        },
+        "properties": {
+          "Name": "Classroom and Office Building"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42366267095503,
+            37.36763873972721
+          ]
+        },
+        "properties": {
+          "Name": "Academic Office Annex"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42295849946261,
+      37.36774012323711
+          ]
+        },
+        "properties": {
+          "Name": "Student Services Building"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42196786779051,
+      37.36728517055762
+          ]
+        },
+        "properties": {
+          "Name": "Social Science and Managment Building"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42234903252103,
+            37.366575800608544
+          ]
+        },
+        "properties": {
+          "Name": "Science and Engineering Building 1"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42209789361475,
+            37.365637147653345
+          ]
+        },
+        "properties": {
+          "Name": "Science and Engineering Building 2"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42115103206388,
+            37.36621976121832
+          ]
+        },
+        "properties": {
+          "Name": "Central Plant Water Tank"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42082183647261,
+            37.36650836903381
+          ]
+        },
+        "properties": {
+          "Name": "Central Utilities Plant"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42072681094034,
+            37.36618739391206
+          ]
+        },
+        "properties": {
+          "Name": "Telecom Building"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42028901473903,
+            37.36648139638746
+          ]
+        },
+        "properties": {
+          "Name": "Campus Police"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42007860106098,
+            37.366999269505015
+          ]
+        },
+        "properties": {
+          "Name": "Facilities Services"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.4207824475211,
+      37.36435280911412
+          ]
+        },
+        "properties": {
+          "Name": "EH&S Facility"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42258855399817,
+            37.364050836725696
+          ]
+        },
+        "properties": {
+          "Name": "Greenhouse"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42422843969162,
+      37.36184005957139
+          ]
+        },
+        "properties": {
+          "Name": "Basketball courts"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.42450892922344,
+      37.36229170065991
+          ]
+        },
+        "properties": {
+          "Name": "Tennis courts"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.429453,
+      37.361598
+          ]
+        },
+        "properties": {
+          "Name": "Aquatic Center"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.430447,
+            37.36262
+          ]
+        },
+        "properties": {
+          "Name": "Bobcat Field"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -120.429066,
+      37.36262
+          ]
+        },
+        "properties": {
+          "Name": "Recreation Field"
+        }
+      }  
+    ]
+  };
+
